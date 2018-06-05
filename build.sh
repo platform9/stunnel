6634aa1988313eefb2a18d69e77da00c10b5c7dd
Bich Le <leb@platform9.net>
2018-06-05 16:17:15 -0700

    add patch to insert logging around I/O operations

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 set -e
-STUNNEL_RELEASE=${STUNNEL_RELEASE:-"stunnel-5.43"}
+STUNNEL_RELEASE=${STUNNEL_RELEASE:-"stunnel-5.46"}
 STUNNEL_URL=${STUNNEL_URL:-"https://www.stunnel.org/downloads/${STUNNEL_RELEASE}.tar.gz"}
 
 wget ${STUNNEL_URL}
 tar xf stunnel-*.gz
-
+patch -p0 < pf9.patch
 mkdir build
 cd ${STUNNEL_RELEASE}
 ./configure
